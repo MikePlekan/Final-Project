aea5851b18138779b06fb32bc69ffe37f3c19054
Spencer-Moon <sj04moon@siena.edu>
2021-04-28 14:58:48 -0400

    Possible Chess structure

---
+++ Square.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Square here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Square
+{
+    private int color;
+    public Piece piece;
+}
