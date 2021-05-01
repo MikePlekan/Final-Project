e84390d53000f2617c2f63bc91e80ab11a3a4c78
LukeJennings1729 <lg04jenn@siena.edu>
2021-05-01 12:24:52 -0400

    starting piece heirarchy

--- Board.java
+++ Board.java
@@ -1,32 +1,34 @@
 
 /**
- * Write a description of class Board here.
+ * This class is used to represent the logical representation of a board, rather than
+ * including JavaFX comments, this class is used to help pieces calculate legal moves,
+ * 
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Luke Jennings
+ * @version Spring2021
  */
 public class Board
 {
-    public Square[][] squares;
-    public Piece[][] pieces;
-    public Board()
-    {
-        squares = new Square[8][8];
-        pieces = new Piece[8][8];
-        
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                squares[i][j] = new Square(i + 1, j + 1);
-            }
-        }
-        
-        setBoard();
-    }
+    /* So I did some research, I personally think that storing all pieces in an array of
+     * 64 piece objects is way easier to calculate valid moves than an 8x8 double array
+     * The reason being is that instead of having to return both a rank (row) and file (column)
+     * for every single calculation, we could instead use a single number to represent each square.
+     * 
+     * Also this would make a method that could be used to load board states easier to program, as the
+     * current standard FEN takes a similar approach when loading in pieces.
+     * As such 0 will represent the square a8, and 63 will represent h1.
+     */
     
-    public void setBoard()
-    {
-        pieces[1][2] = new Pawn(1, 2);
-    }
+    protected Piece[] board = new Piece[64];
+    
+    public static String[] notation = {"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+        "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+        "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+        "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+        "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+        "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+        "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"};
+        
+    
 }
--- Piece.java
+++ Piece.java
@@ -1,11 +1,61 @@
-import java.util.ArrayList;
+
 /**
- * Abstract class Piece - write a description of the class here
+ * Abstract class used to represent a chess man
  *
- * @author (your name here)
- * @version (version number or date here)
+ * @author Luke Jennings
+ * @version Spring 2021
  */
-abstract public class Piece
+public abstract class Piece
 {
-    abstract public ArrayList<TwoVal> validMoves();
+    //If a piece's boolean color = false, then it is a white piece, if color = true, it is black.
+    protected Boolean color;
+    
+    //Used to track what square a piece in on
+    protected int currentSquare;
+
+    
+    /* I don't know if this is necessary right now, but these arrays are to calculate the offsets of each direction a piece can move. 
+     * For example, a bishop if on the a8 square (0), wants to move to b7, it must check if square (9) is a valid square.
+     */
+    //public static int[] diagonal = {-9,9,-7,7};
+    
+    //public static int[] horizontal = {-8, 8, -1,1};
+    
+    /**
+     * Returns the color of a piece object as a boolean. If returns false, then it is a white piece, if black it returns true.
+     * 
+     * @return false if piece is white, true if piece is black.
+     */
+    public boolean getColor(){
+        return color;
+    }
+    
+    /**
+     * Returns the color of a piece object as a string. 
+     * If returns "white", then it is a white piece, if it returns "black", then it is a black piece.
+     * 
+     * @return "white" if piece is white, "black" if piece is black.
+     */
+    public String getColorStr(){
+        if(color){
+            return "black";
+        } else {
+            return "white";
+        }
+    }
+    
+    abstract public String getPieceStr();
+    
+    /**
+     * Note: This method may be uncessary.
+     * Returns the current square as given by the representation of the board class 
+     */
+    public int getSquare(){
+        return currentSquare;
+    }
+        
+    public String getSquareStr(){
+        return Board.notation[currentSquare];
+    }
+    
 }
