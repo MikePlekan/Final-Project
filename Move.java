bf79769e94e2f84eede283422e0a815f35dce46a
LukeJennings1729 <lg04jenn@siena.edu>
2021-05-13 10:44:05 -0400

    i forgot move.java

---
+++ Move.java
@@ -0,0 +1,61 @@
+
+/**
+ * Write a description of class Move here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Move
+{
+    // instance variables - replace the example below with your own
+    String s = "";
+    protected int initialSquare;
+    protected int targetSquare;
+    protected Piece pieceCaptured;
+    protected boolean previouslyMoved = false;
+
+    public Move(Board b,String s, int initialSquare,int targetSquare, Piece pieceCaptured){
+        if(!s.equals("P")){
+            this.s = s;
+        } else {
+            s = "";
+        }
+        if(b.board[initialSquare] instanceof MovedPiece){
+            
+            MovedPiece p = (MovedPiece) b.board[initialSquare];
+            previouslyMoved = p.moved;
+        
+        }
+        this.initialSquare = initialSquare;
+        this.targetSquare = targetSquare;
+        this.pieceCaptured = pieceCaptured;
+
+    }
+
+    @Override
+    public String toString(){
+        if(pieceCaptured != null){
+            return s + Board.notation[initialSquare] + "x" + Board.notation[targetSquare];
+        } else {
+            return s + Board.notation[initialSquare] + "-" + Board.notation[targetSquare];
+        }
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Move){
+            Move other = (Move) o;
+            if(this.initialSquare == other.initialSquare && this.targetSquare == other.targetSquare){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+           throw new ClassCastException("not a object of type Move");
+        }
+        
+    
+        
+    }
+
+}
