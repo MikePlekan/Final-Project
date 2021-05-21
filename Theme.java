23a55ae7d10dd1a77f3665ee59f10ac8eef33940
Spencer-Moon <sj04moon@siena.edu>
2021-05-21 09:41:58 -0400

    Added valid move constants to Theme

--- Theme.java
+++ Theme.java
@@ -10,8 +10,8 @@ public class Theme
 {
     public static final Color[] FIRST_COLORS = {new Color(255, 228, 179), Color.WHITE, new Color(255, 249, 209), new Color(255, 249, 209), new Color(247, 204, 255), Color.WHITE};
     public static final Color[] SECOND_COLORS = {new Color(158, 107, 71), new Color(100, 100, 100), new Color(255, 142, 51), new Color(52, 173, 82), new Color(141, 56, 201), new Color(255, 184, 230)};
-    public Color validMove1;
-    public Color validMove2;
+    public Color validMoveLight = new Color(255, 84, 84);
+    public Color validMoveDark = new Color(219, 39, 39);
     private static Random rand = new Random();
 
     public synchronized static Color getRandomMono()
